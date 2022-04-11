7b9d77c0c471c0ed5494683b945ee2a23ac58429
JBowersock <jbowersock22@hotmail.com>
2022-04-11 19:06:19 -0400

    Create ClassInheritance.py

---
+++ ClassInheritance.py
@@ -0,0 +1,27 @@
+class Author: #parent class
+    #define attributes of class
+    name: 'George R. R. Martin'
+    born: '9/20/1948'
+
+class Novel1(Author): #child class, with inheritance.
+    #define attributes of class
+    title: 'A Game of Thrones'
+    publisher: 'Bantam Spectra'
+    pages: 694
+    genre: 'Fantasy'
+
+class Novel2(Author): #child class, with inheritance.
+    #define attributes of class
+    title: 'Fire & Blood'
+    publisher: 'Bantam Books'
+    pages: 736
+    genre: 'Fantasy'
+
+
+##INHERITANCE SUBMISSION ASSIGNMENT
+##
+##Create two classes that inherit from another class.
+##
+##Ensure each child has at least two of their own attributes.
+##
+##Add comments throughout your Python explaining your code.
